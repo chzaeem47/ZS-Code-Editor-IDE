c8a355e6bf58a0faf08027d31a002ee022b37a92
chzaeem47 <zaeemshahidak47@gmail.com>
2026-09-24 19:25:18 +0500

    new routes for credits deduct or addition

--- backend/services/auth/routes/auth.routes.js
+++ backend/services/auth/routes/auth.routes.js
@@ -3,6 +3,8 @@ import { Router } from "express";
 import { signup, login, googleLogin, logout } from "../controllers/auth.controller.js";
 import { signupSchema, loginSchema, validate } from "../middlewares/auth.validator.js";
 import { signupRateLimiter, loginRateLimiter } from "../middlewares/rateLimiting.js";
+import { addCredits, deductCredits } from "../controllers/user.controller.js";
+import { protect } from "../middlewares/protect.js";
 
 const router = Router();
 
@@ -26,6 +28,9 @@ router.post("/login", loginRateLimiter, validate(loginSchema), login);
 router.post("/google", googleLogin);
 
 router.get('/logout',logout)
+router.post('/user/deduct-credits',protect,deductCredits)
+router.post('/user/add-credits',protect,addCredits)
+
 
 
 export default router;
